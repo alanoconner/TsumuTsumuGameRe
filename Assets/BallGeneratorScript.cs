6103821c29f7d15ea382dcaecc7df3b6cf36b000
Alan <86943620+alanoconner@users.noreply.github.com>
2021-10-03 17:31:34 +0900

    -changedToVegetables

--- Assets/BallGeneratorScript.cs
+++ Assets/BallGeneratorScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BallGeneratorScript : MonoBehaviour
 {
     [SerializeField] GameObject ballPrefab = default;
-    [SerializeField] Sprite[] balls = default;
+    [SerializeField] GameObject[] balls = default;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,11 +23,13 @@ public class BallGeneratorScript : MonoBehaviour
         for(int i = 0; i < count; i++)
         {
             Vector2 position = new Vector2(Random.Range(-0.2f,0.3f),8f);
+            int ballcolor = Random.Range(0, balls.Length);
+            ballPrefab = balls[ballcolor];
             GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
 
-            int ballcolor = Random.Range(0, balls.Length);
-            ball.GetComponent<SpriteRenderer>().sprite = balls[ballcolor];
-            ball.GetComponent<BallScript>().id = ballcolor;
+            
+            //ball = balls[ballcolor];
+            //ball.GetComponent<BallScript>().id = ballcolor;
 
             yield return new WaitForSeconds(0.03f);
         }
